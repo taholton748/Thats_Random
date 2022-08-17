1f20fe8409882e2273932ac8449ddba874e70404
Medici <107539009+taholton748@users.noreply.github.com>
2022-08-16 22:57:58 -0600

    added values to variables in script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var confirmLower;
 var confirmUpper;
 var confirmNumber;
 var confirmSpecial;
+//var userChoices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"];
@@ -23,3 +24,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Start Function
+

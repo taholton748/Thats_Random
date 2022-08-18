989d5735fe81e45ca8cb6ef1d221f665451c123f
Medici <107539009+taholton748@users.noreply.github.com>
2022-08-18 02:35:47 -0600

    added excessive conditionals to script.js

--- script.js
+++ script.js
@@ -104,14 +104,6 @@ var special = [
   Once complete, then display the password to the page
 */
 
-// Create password and display in the page
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
 //Start function to generate password
 function generatePassword() {
   
@@ -125,50 +117,98 @@ function generatePassword() {
     passwordLength = prompt("How many characters do you desire between 8 and 128?");
   }
   
-  // Prompt for lower case confirmation
+  // Prompts for confirmation
   var confirmLower = confirm("Do you want to include lowercase letters?");
-  
-  // Conditional for lower case selection
-  if (confirmLower) {
-    userChoices = userChoices.concat(lowerCase);
-    console.log('it worked');
-  }
-  
-
-  // //Prompt for upper case confirmation
   var confirmUpper = confirm("Do you want to include uppercase letters?");
-  // Conditional for upper case selection
-  if (confirmUpper) {
+  var confirmNumber = confirm("Do you want to include numbers?");
+  var confirmSpecial = confirm("Do you want to include symbols?");
+
+  // Conditional for selection of all 4
+  if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = userChoices.concat(lowerCase, upperCase, numbers, special);
+  }
+  
+  // Conditional for no lowercase letters
+  else if (confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = userChoices.concat(upperCase, numbers, special);
+  }
+
+  // Conditional for no uppercase letters
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userChoices = userChoices.concat(lowerCase, numbers, special);
+  }
+
+  // Conditional for no numbers
+  else if (confirmLower && confirmUpper && confirmSpecial) {
+    userChoices = userChoices.concat(lowerCase, upperCase, special);
+  }
+
+  // Conditional for no special characters
+  else if (cofirmLower && confirmUpper & confirmNumber) {
+    userChoices = userChoices.concat(lowerCase, upperCase, numbers);
+  }
+
+  // Upper and lower case selected
+  else if (confirmLower && confirmUpper) {
+    userChoices = userChoices.concat(lowerCase, upperCase);
+  }
+
+  // Upper case and numbers selected
+  else if (confirmUpper && confirmNumber) {
+    userChoices = userChoices.concat(upperCase, numbers);
+  }
+
+  // Upper case and symbols selected
+  else if (confirmUpper && confirmSpecial) {
+    userChoices = userChoices.concat(upperCase, special);
+  }
+
+  //Lower case and numbers selected
+  else if (confirmLower && confirmNumber) {
+    userChoices = userChoices.concat(lowerCase, numbers);
+  }
+
+  //Lower case and special characters selected
+  else if (confirmLower && confirmSpecial) {
+    userChoices = userChoices.concat(lowerCase, special);
+  }
+
+  // Numbers and special characters selected
+  else if (confirmNumber && confirmSpecial) {
+    userChoices = userChoices.concat(numbers, special);
+  }
+
+  // Only lowercase
+  else if (confirmLower) {
+    userChoices = userChoices.concat(lowerCase);
+  }
+
+  //Only uppercase
+  else if (confirmUpper) {
     userChoices = userChoices.concat(upperCase);
-    console.log(userChoices);
   }
 
-  // //Prompt for numbers confirmation
-  let confirmNumber = confirm("Do you want to include numbers?");
-  // Conditional for numbers selection
-  if (confirmNumber) {
+  //Only numbers
+  else if (confirmNumber) {
     userChoices = userChoices.concat(numbers);
-    console.log(userChoices);
   }
 
-  // //Prompt or special characters confirmation
-  let confirmSpecial = confirm("Do you want to include symbols?");
-  // Conditional for special characters selection
-  if (confirmSpecial) {
+  //Only special characters
+  else if (confirmSpecial) {
     userChoices = userChoices.concat(special);
-    console.log(userChoices, 'still working')
   }
 }
   var randomPassword ='';
-  //10
-  //UC = true ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  //LC = true ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  // SC = false
-  // Num = true [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   for (var i = 0; i < passwordLength; i++) {
     var randomPassword = userChoices[Math.floor(Math.random() * userChoices.length)];
   }
-  
+// Create password and display in the page
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
